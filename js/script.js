620bb3f452c7d800f1ec0f1ab68cf73789e9b326
lgs <devnull@localhost>
2012-06-21 23:29:25 +0200

    Use colorbox to show some information as popups. Basically the public ssh key and the credits section

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
-/* Author:
+(function ($) {
+    $(document).ready(function () {
 
-*/
+        $("a.popupTrigger").colorbox({
+            inline: true,
+            maxWidth: "60%",
+            maxHeight: "80%",
+            onOpen: function () {
+                $($(this).attr("href")).show();
+            },
+            onClosed: function () {
+                $($(this).attr("href")).hide();
+            }
+        });
 
-
-
-
-
+    });
+}(jQuery));
