a19310c2ac120c6c85d1a7e2422c624c5c93e562
lgs <devnull@localhost>
2013-09-04 23:54:13 +0200

    Upgrade to Bootstrap 3.0

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 (function ($) {
     $(document).ready(function () {
 
-        $("a.popupTrigger").colorbox({
-            inline: true,
-            maxWidth: "60%",
-            maxHeight: "80%",
-            onOpen: function () {
-                $($(this).attr("href")).show();
-            },
-            onClosed: function () {
-                $($(this).attr("href")).hide();
-            }
-        });
-
     });
 }(jQuery));
